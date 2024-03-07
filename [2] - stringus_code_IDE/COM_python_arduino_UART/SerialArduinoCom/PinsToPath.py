b204569353d75a7fb89b295be24819f54fc5533a
interface-graphique2 <dosg0801@usherbrooke.ca>
2024-03-07 11:57:02 -0500

    Proxy gcode generator- can extract pins from csv file

--- [2] - stringus_code_IDE/COM_python_arduino_UART/SerialArduinoCom/PinsToPath.py
+++ [2] - stringus_code_IDE/COM_python_arduino_UART/SerialArduinoCom/PinsToPath.py
@@ -7,16 +7,16 @@ class Path:
     def __init__(self, csvfilepath, numPins):
         self.csvfile = pd.read_csv(csvfilepath)
         self.numPins = numPins
-        self.angles = np.linspace(0, 2 * np.pi, numPins + 1)
+        self.angles = np.linspace(0, 360, numPins + 1)
 
         self.pins = np.array((self.csvfile.p1, self.csvfile.p2))
-        self.pinsInRads = np.zeros_like(self.pins).astype(np.float32)
-        self.convertPinsToRads()
+        self.pinsInDeg = np.zeros_like(self.pins).astype(np.float32)
+        self.convertPinsToDeg()
 
-    def convertPinsToRads(self):
+    def convertPinsToDeg(self):
         for i in range(np.shape(self.pins)[1]):
-            self.pinsInRads[0][i] = self.angles[self.pins[0][i]]
-            self.pinsInRads[1][i] = self.angles[self.pins[1][i]]
+            self.pinsInDeg[0][i] = self.angles[self.pins[0][i]]
+            self.pinsInDeg[1][i] = self.angles[self.pins[1][i]]
 
 
 if __name__ == "__main__":
--- [2] - stringus_code_IDE/COM_python_arduino_UART/SerialArduinoCom/main.py
+++ [2] - stringus_code_IDE/COM_python_arduino_UART/SerialArduinoCom/main.py
@@ -1,8 +1,10 @@
 import serial
 import time
+from PinsToPath import *
 
 arduino = serial.Serial('COM4', 115200, timeout=1)
 time.sleep(2)  # Temps pour que la connection se fasse
+
 
 def envoie_commande(commande):
     print(f"Python: {commande}")
@@ -14,17 +16,23 @@ def envoie_commande(commande):
             if reponse == '1':  # Attend le signal du arduino
                 break  # Sort de la loop pour envoyer une autre commande
 
-commandes = [
-    "{C1 56 236}",
-    "{C1 11 225}",
-    "{C1 62 203}",
-    "{C1 68 206}",
-    "{C1 30 210}",
-]
+
+# commandes = [
+#     "{C1 56 236}",
+#     "{C1 11 225}",
+#     "{C1 62 203}",
+#     "{C1 68 206}",
+#     "{C1 30 210}",
+# ]
+
+path = Path("StringUS/[1] - ImageProcessing/outputs/shronk.csv", 300)
+
+commandes = []
+for pin in path.pinsInDeg[1]:
+    commandes.append("C2 %f" % pin)
 
 for cmd in commandes:
     envoie_commande(cmd)
-    time.sleep(0.01)  #Pour laisser le temps au arduino de calculer
-    
-#arduino.close()  # Si toute les commandes sont complété
+    time.sleep(0.01)  # Pour laisser le temps au arduino de calculer
 
+# arduino.close()  # Si toute les commandes sont complété
