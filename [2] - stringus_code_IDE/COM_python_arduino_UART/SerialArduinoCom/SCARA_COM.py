42f1f67851d93ca2c20a4674a620b43e4131f28a
interface-graphique2 <dosg0801@usherbrooke.ca>
2024-03-21 17:08:32 -0400

    merged useless class and clean up

--- [2] - stringus_code_IDE/COM_python_arduino_UART/SerialArduinoCom/SCARA_COM.py
+++ [2] - stringus_code_IDE/COM_python_arduino_UART/SerialArduinoCom/SCARA_COM.py
@@ -1,6 +1,6 @@
 import serial
 import time
-from PinsToPath import *
+import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -26,10 +26,10 @@ class SCARA_COM:
         self.seq0 = [0]
 
         # TODO CHECK TON CRISSE DE PORT
-        self.arduino = serial.Serial('COM%i'%COM, 115200, timeout=1)
+        self.arduino = serial.Serial('COM%i' % COM, 115200, timeout=1)
         time.sleep(2)  # Temps pour que la connection se fasse
 
-    def envoie_commande(self,commande):
+    def envoie_commande(self, commande):
         print(f"Python: {commande}")
         self.arduino.write(commande.encode())
         while True:
@@ -39,6 +39,17 @@ class SCARA_COM:
                 if reponse == '1':  # Attend le signal du arduino
                     break  # Sort de la loop pour envoyer une autre commande
 
+    def readThreadedCSV(self, csvfilepath, numPins):
+        self.csvfile = pd.read_csv(csvfilepath)
+        self.numPins = numPins
+        self.angles = np.linspace(0, 4096, numPins + 1, dtype=int)
+
+        self.pins = np.array((self.csvfile.p1, self.csvfile.p2))
+        self.pinsInPulse = np.zeros_like(self.pins).astype(np.float32)
+
+        for i in range(np.shape(self.pins)[1]):
+            self.pinsInPulse[0][i] = self.angles[self.pins[0][i]]
+            self.pinsInPulse[1][i] = self.angles[self.pins[1][i]]
 
     def readPos(self):
         self.arduino.write('{T1}'.encode())
@@ -54,18 +65,6 @@ class SCARA_COM:
                 self.ActualPos['table'].append(int(response))
                 print(f"position table : {response}\n")
                 break
-
-
-    def manual_calibration_dxl(self):
-        self.arduino.write('{T0}'.encode())
-        while True:
-            if self.arduino.in_waiting > 0:
-                reponse = self.arduino.readline().decode().strip()
-                if reponse == '1':
-                    print("torque has been disabled, you are free to move the arms ")
-                    while True:
-                        input("press enter to read current position : \n")
-                        self.readPos()
 
     def readSeq(self):
         self.arduino.write('{T2}'.encode())
@@ -99,12 +98,12 @@ class SCARA_COM:
                     print("torque has been disabled, you are free to move the arms ")
                     self.readSeq()
 
-    def start_run(self,filepath,numPins):
+    def start_run(self, filepath, numPins):
 
-        path = Path(csvfilepath=filepath, numPins=numPins)
+        self.readThreadedCSV(filepath, numPins)
         commandes = []
 
-        for pin in path.pinsInPulse[0]:
+        for pin in self.pinsInPulse[0]:
             commandes.append("{C2 %d}" % pin)
             self.Pos["table"].append(pin)
             # self.Pos["left"].append(seq0[4][0])
@@ -129,7 +128,6 @@ class SCARA_COM:
         line3, = ax.plot(x, et, 'g-')
 
         for i, cmd in enumerate(commandes):
-
             self.envoie_commande(cmd)
             self.readPos()
 
@@ -141,7 +139,7 @@ class SCARA_COM:
             # line1.set_ydata(eg)
             # line2.set_ydata(ed)
 
-            ax.set_xlim(0, i+1)
+            ax.set_xlim(0, i + 1)
             fig.canvas.draw()
             plt.draw()
             fig.canvas.flush_events()
